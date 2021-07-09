f8eca38d2d28b73ed4e40b37991faf403e473dba
“Sebik <waffilka@gmail.comSebik Georgegit config --global user.email waffilka@gmail.com>
2021-07-09 09:49:21 +0300

    add second implementation for SIN

--- CppLesson/Sin.cpp
+++ CppLesson/Sin.cpp
@@ -2,7 +2,7 @@
 
 double Sin::GetSinWithEpsilon(double x, double a, int k, double eps)
 {
-	if (abs(a) >= eps)
+	if (abs(a) > eps)
 	{
 		return a + GetSinWithEpsilon(x, a * (-pow(x, 2) / (2 * k + 3) / (2 * k + 2)), k++, eps);
 	}
@@ -10,7 +10,48 @@ double Sin::GetSinWithEpsilon(double x, double a, int k, double eps)
 	return a;
 }
 
+double Sin::_getSinI(double x, int k)
+{
+	return pow(-1, k % 2) * pow(x, 2 * k + 1) / factorial(2 * k + 1);
+}
+
 double Sin::GetSinWithEpsilon(double x, double eps)
 {
 	return GetSinWithEpsilon(x, x, 0, eps);
+}
+
+double Sin::GetSinWithEpsilonV2(double x, double eps)
+{
+	double value = 0;
+	int index = 0;
+	double result = 0;
+
+	do {
+		value = _getSinI(x, index);
+		result += value;
+		index++;
+
+	} while (abs(value) > eps);
+
+	return result;
+}
+
+int Sin::factorial(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return 1;
+	}
+
+	return n * factorial(n - 1);
+}
+
+int Sin::factorial(int curRes, int n)
+{
+	if (n)
+	{
+		return factorial(curRes * n, n - 1);
+	}
+
+	return curRes;
 }
--- CppLesson/Sin.h
+++ CppLesson/Sin.h
@@ -5,7 +5,11 @@
 class Sin
 {
 	double GetSinWithEpsilon(double x, double a, int k, double eps);
+	double _getSinI(double x, int k);
 public:
 	double GetSinWithEpsilon(double x, double eps = 0.0001);
+	double GetSinWithEpsilonV2(double x, double eps = 0.0001);
+	int factorial(int n);
+	int factorial(int curRes, int n);
 };
 
